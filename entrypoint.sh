851b9c095110e4abde03694390413488eb02d04b
Paul Dann <pgdann@gmail.com>
2025-07-10 00:54:27 +0100

    Make home directory path consistent

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ host_gid=$(stat -c %g .)
 group_name=$((getent group $host_gid || echo) | cut -d: -f1)
 if [[ ! $group_name ]]; then
   group_name=user
-  groupadd -g $host_gid user
+  groupadd -g $host_gid $group_name
 fi
 
 user_name=$((getent passwd $host_uid || echo) | cut -d: -f1)
@@ -25,8 +25,10 @@ if [[ ! $user_name ]]; then
   useradd -mg $host_gid -u $host_uid $user_name 2> /dev/null
 fi
 
-# If the home directory is mounted as a volume, it will initially be root-owned.
-chown $host_uid:$host_gid /home/$user_name
+# It's helpful to use the same home directory regardless of base image.
+HOME=/home/user
+mkdir -p $HOME
+chown $host_uid:$host_gid /home/user
 
 if [[ ! -f /etc/sudoers.d/user ]]; then
   echo "$user_name ALL=(ALL:ALL) ALL" > /etc/sudoers.d/user
@@ -34,7 +36,6 @@ if [[ ! -f /etc/sudoers.d/user ]]; then
   echo "$user_name:secret" | chpasswd
 fi
 
-HOME=/home/$user_name
 exec setpriv \
   --reuid $host_uid \
   --regid $host_gid \
