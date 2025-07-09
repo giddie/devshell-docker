d3eb5c0b9524a11318f038b54a09573eaf5ab5dc
Paul Dann <pgdann@gmail.com>
2025-07-09 23:43:38 +0100

    Add prompt to git clone my shell

--- entrypoint-user.sh
+++ entrypoint-user.sh
@@ -2,11 +2,22 @@
 
 set -euo pipefail
 
-if [[ ! -d ~/.zprezto && -d /usr/local/lib/prezto/runcoms ]]; then
-  ln -s /usr/local/lib/prezto ~/.zprezto
+if [[ ! -d ~/.zprezto ]]; then
+  if [[ -d /usr/local/lib/prezto/runcoms ]]; then
+    ln -s /usr/local/lib/prezto ~/.zprezto
+  else
+    echo -n "Would you like a nice shell prompt? [Y/n]: "
+    read -r yn
+    case "$yn" in
+      "" | [Yy]*) git clone --recursive https://github.com/giddie/prezto.git ~/.zprezto; break ;;
+      *) ;;
+    esac
+  fi
+
   for dotfile in ~/.zprezto/runcoms/z*; do
     ln -sf $dotfile ~/.$(basename $dotfile)
   done
+
   mkdir -p ~/.zprezto-extra/misc
   touch ~/.zprezto-extra/misc/init.zsh
 fi
